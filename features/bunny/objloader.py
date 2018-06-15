1cbf143e2603442034c8afe2d6035ed8e519a485
eggplant673 <39640684+eggplant673@users.noreply.github.com>
2018-06-15 21:12:42 +0800

    Update objloader.py

--- features/bunny/objloader.py
+++ features/bunny/objloader.py
@@ -1,1 +1,28 @@
-ssss
+import numpy as np
+import pygame, OpenGL
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from OpenGL.GLUT.freeglut import *
+
+
+
+
+def MTL(fdir,filename):
+    contents = {}
+    mtl = None
+    for line in open(fdir+filename, "r"):
+        if line.startswith('#'): continue
+        values = line.split()
+        if not values: continue
+        if values[0] == 'newmtl':
+            mtl = contents[values[1]] = {}
+        elif mtl is None:
+            raise ValueError( "mtl file doesn't start with newmtl stmt" )
+        elif values[0] == 'map_Kd':
+            # load the texture referred to by this declaration
+            mtl[values[0]] = values[1]
+            surf = pygame.image.load(fdir+mtl['map_Kd'])
+            image = pygame.image.tostring(surf, 'RGBA', 1)
+            ix, iy = surf.get_rect().size
